915d11b7a7b761f0da2b5f316130efc75e8f6c65
Gisela Elia <lucena.gisela@gmail.com>
2026-05-31 12:58:28 -0400

    renames CTA folder

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import Adoption from "@/components/Adoption";
 import Main from "@/components/Main";
 import Service from "@/components/Services";
-import Cta from "../../src/components/Cta/index";
+import Cta from "@/components/CTA";
 export default function Home() {
   return (
     <>
